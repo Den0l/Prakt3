3253f9bb30cbbf0a7073cbca2257d8ce0902ba70
Den0l <136687221+Den0l@users.noreply.github.com>
2024-02-15 19:08:41 +0300

    Add files via upload

---
+++ ConsoleApplication3/ConsoleApplication3/ConsoleApplication3.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    float mas[3][5];
+    float res;
+    for (int i = 0; i < 3; i ++)
+    {
+        for (int j = 0; j < 5; j++)
+        {
+            cin >> mas[i][j];
+        }
+
+    }
+    cout << endl;
+    for (int g = 0; g < 3; g++)
+    {
+        res = 0;
+        for (int f = 0; f < 5; f++)
+        {
+             res += mas[g][f];
+        }
+        cout << res / 5 << endl;
+    }
+
+}
+
